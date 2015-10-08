8b1d8463814c16ac1ac9d36719c6d7f04276fce6
Javier Prieto <jprieton@gmail.com>
2015-10-08 12:05:44 -0430

    Breadcrumbs

--- helpers/breadcrumb.php
+++ helpers/breadcrumb.php
@@ -1,66 +1,72 @@
 <?php
+
 /**
- *
- * @return type
+ * @return string
  */
-function the_breadcrumb() {
-	if (is_home()) return;
+function get_the_breadcrumb() {
+	if ( is_home() ) return;
 
-	$items = array('Inicio' => home_url());
+	$items = array( 'Inicio' => home_url() );
 
-	if (is_singular()) {
+	if ( is_singular() ) {
 
-		$post_ancestors = array_reverse((array) get_post_ancestors(get_the_ID()));
+		$post_ancestors = array_reverse( (array) get_post_ancestors( get_the_ID() ) );
 
-		$post_type = get_post_type(get_the_ID());
-		$post_type_object = get_post_type_object($post_type);
+		$post_type = get_post_type( get_the_ID() );
+		$post_type_object = get_post_type_object( $post_type );
 
-		if ($post_type_object->has_archive) {
-			$items[$post_type_object->labels->name] = get_post_type_archive_link($post_type);
+		if ( $post_type_object->has_archive ) {
+			$items[$post_type_object->labels->name] = get_post_type_archive_link( $post_type );
 		}
 
-		foreach ($post_ancestors as $ancestor_id) {
-			$post_ancestor = get_post($ancestor_id);
-			$items[$post_ancestor->post_title] = get_permalink($post_ancestor->ID);
+		foreach ( $post_ancestors as $ancestor_id ) {
+			$post_ancestor = get_post( $ancestor_id );
+			$items[$post_ancestor->post_title] = get_permalink( $post_ancestor->ID );
 		}
 		$active = get_the_title();
-	} elseif (is_post_type_archive()) {
+	} elseif ( is_post_type_archive() ) {
 
 		$post_type = get_post_type();
-		$post_type_object = get_post_type_object($post_type);
+		$post_type_object = get_post_type_object( $post_type );
 		$active = $post_type_object->labels->name;
-	} elseif (is_tax()) {
+	} elseif ( is_tax() ) {
 
-		if (!is_taxonomy_hierarchical(get_query_var('taxonomy'))) {
-			$term_object = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy'));
+		if ( !is_taxonomy_hierarchical( get_query_var( 'taxonomy' ) ) ) {
+			$term_object = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
 			$active = $term_object->name;
 		} else {
-			$taxonomy = get_query_var('taxonomy');
-			$term_object = get_term_by('slug', get_query_var('term'), $taxonomy);
+			$taxonomy = get_query_var( 'taxonomy' );
+			$term_object = get_term_by( 'slug', get_query_var( 'term' ), $taxonomy );
 			$active = $term_object->name;
 
 			$hierarchy_terms = array();
 			$term_ancestor_id = (int) $term_object->parent;
-			while ($term_ancestor_id != 0) {
-				$_term = get_term($term_ancestor_id, $taxonomy);
+			while ( $term_ancestor_id != 0 ) {
+				$_term = get_term( $term_ancestor_id, $taxonomy );
 				$hierarchy_terms[] = $_term;
 				$term_ancestor_id = (int) $_term->parent;
 			}
-			$hierarchy_terms = array_reverse((array) $hierarchy_terms);
+			$hierarchy_terms = array_reverse( (array) $hierarchy_terms );
 
-			foreach ($hierarchy_terms as $term) {
-				$items[$term->name] = get_term_link($term);
+			foreach ( $hierarchy_terms as $term ) {
+				$items[$term->name] = get_term_link( $term );
 			}
 		}
 	}
 
 	$breadcrumb = '<ol class="breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList">';
-	foreach ($items as $item => $value) {
-		$breadcrumb .= sprintf('<li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a href="%s" itemprop="item"><span itemprop="name">%s</span></a></li>', $value, $item);
+	foreach ( $items as $item => $value ) {
+		$breadcrumb .= sprintf( '<li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a href="%s" itemprop="item"><span itemprop="name">%s</span></a></li>', $value, $item );
 	}
-	$breadcrumb .= (!empty($active)) ? sprintf('<li class="active" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">%s</li>', $active) : '';
+	$breadcrumb .= (!empty( $active )) ? sprintf( '<li class="active" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">%s</li>', $active ) : '';
 	$breadcrumb .= '</ol>';
 
-	echo $breadcrumb;
+	return $breadcrumb;
 }
 
+/**
+ * Shows the breadcrumb
+ */
+function the_breadcrumb() {
+	echo get_the_breadcrumb();
+}
