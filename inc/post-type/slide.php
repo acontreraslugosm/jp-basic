13081db95ee47cee8dceb2d739f590628c555eef
jprieton <jprieton@gmail.com>
2015-10-17 00:41:21 -0430

    Slide Post Type

--- inc/post-type/slide.php
+++ inc/post-type/slide.php
@@ -6,18 +6,18 @@
 defined( 'TEXT_DOMAIN' ) || define( 'TEXT_DOMAIN', 'jp-basic' );
 
 /**
- * Register People Post Type
+ * Register Slide Post Type
  */
 add_action( 'init', function () {
 
 	$labels = array(
-		'name'                => _x( 'People', TEXT_DOMAIN ),
-		'singular_name'       => _x( 'Person', TEXT_DOMAIN ),
-		'menu_name'           => __( 'People', TEXT_DOMAIN ),
-		'name_admin_bar'      => __( 'Person', TEXT_DOMAIN ),
+		'name'                => __( 'Slides', TEXT_DOMAIN ),
+		'singular_name'       => __( 'Slide', TEXT_DOMAIN ),
+		'menu_name'           => __( 'Slides', TEXT_DOMAIN ),
+		'name_admin_bar'      => __( 'Slide', TEXT_DOMAIN ),
 		'parent_item_colon'   => __( 'Parent Item:', TEXT_DOMAIN ),
 		'all_items'           => __( 'All Items', TEXT_DOMAIN ),
-		'add_new_item'        => __( 'Add New Person', TEXT_DOMAIN ),
+		'add_new_item'        => __( 'Add New Item', TEXT_DOMAIN ),
 		'add_new'             => __( 'Add New', TEXT_DOMAIN ),
 		'new_item'            => __( 'New Item', TEXT_DOMAIN ),
 		'edit_item'           => __( 'Edit Item', TEXT_DOMAIN ),
@@ -28,23 +28,23 @@ add_action( 'init', function () {
 		'not_found_in_trash'  => __( 'Not found in Trash', TEXT_DOMAIN ),
 	);
 	$args = array(
-		'label'               => __( 'Person', TEXT_DOMAIN ),
+		'label'               => __( 'Carousel', TEXT_DOMAIN ),
 		'labels'              => $labels,
 		'supports'            => array( 'title', 'editor', 'excerpt', 'thumbnail', ),
 		'hierarchical'        => false,
-		'public'              => true,
+		'public'              => false,
 		'show_ui'             => true,
 		'show_in_menu'        => true,
 		'menu_position'       => 20,
-		'menu_icon'           => 'dashicons-groups',
-		'show_in_admin_bar'   => true,
-		'show_in_nav_menus'   => true,
-		'can_export'          => true,
+		'show_in_admin_bar'   => false,
+		'show_in_nav_menus'   => false,
+		'menu_icon'          => 'dashicons-slides',
 		'has_archive'         => true,		
 		'exclude_from_search' => true,
 		'publicly_queryable'  => false,
+		'rewrite'             => false,
 		'capability_type'     => 'post',
 	);
-	register_post_type( 'person', $args );
+	register_post_type( 'slide', $args );
 
 }, 0 );
