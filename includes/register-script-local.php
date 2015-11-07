0edef9f4b5bb95f6dc4aedf49fef6fc008996750
jprieton <jprieton@gmail.com>
2015-11-07 03:05:47 -0430

    WOW.js Register Scripts/Styles

--- includes/register-script-local.php
+++ includes/register-script-local.php
@@ -1,11 +1,14 @@
 <?php
 
 // Register Script
-add_action('wp_enqueue_scripts', function () {
-
-	// jQuery
-	wp_deregister_script('jquery');
-	wp_register_script('jquery', '/js/jquery.min.js', array(), '2.1.4', false);
-	// Modernizr
-	wp_register_script('modernizr', '/js/modernizr.min.js', array(), '2.8.3', false);
-});
+add_action( 'wp_enqueue_scripts', function () {
+	/* jQuery v2.1.4 */
+	wp_deregister_script( 'jquery' );
+	wp_register_script( 'jquery', '/js/jquery.min.js', array(), '2.1.4', false );
+	/* Modernizr v2.8.3 */
+	wp_register_script( 'modernizr', '/js/modernizr.min.js', array(), '2.8.3', false );
+	/* Bootstrap v3.3.5 */
+	wp_register_script( 'bootstrap', '/js/bootstrap.min.js', array( 'jquery' ), '3.3.5', false );
+	/* WOW.js v1.1.2 */
+	wp_register_script( 'wow', '/js/wow.min.js', array(), '1.1.2', true );
+} );
