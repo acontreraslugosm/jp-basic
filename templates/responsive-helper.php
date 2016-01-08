8271c5e65fb29f59bec387678893d9b5ae272a2e
Javier Prieto <jprieton@gmail.com>
2016-01-08 11:56:00 -0430

    Responsive helper & Modernizr features

--- templates/responsive-helper.php
+++ templates/responsive-helper.php
@@ -3,4 +3,14 @@
 	<div class="visible-sm">sm</div>
 	<div class="visible-md">md</div>
 	<div class="visible-lg">lg</div>
+	<div class="show-modernizr">&raquo;</div>
+	<pre class="modernizr-features hidden"></pre>
 </div>
+<script>
+  $('.show-modernizr').click(function (e) {
+      e.preventDefault();
+      var features = $('html').attr('class');
+      $('.modernizr-features').text(features);
+      $('.modernizr-features').toggleClass('hidden');
+  });
+</script>
