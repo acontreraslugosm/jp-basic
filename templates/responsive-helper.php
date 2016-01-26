32c9848ae68401bbaff25469b6c2c8c0673d6ae2
Javier Prieto <jprieton@gmail.com>
2016-01-26 13:00:57 -0430

    Responsive Helper

--- templates/responsive-helper.php
+++ templates/responsive-helper.php
@@ -1,16 +1,35 @@
 <div class="responsive-helper">
-	<div class="visible-xs">xs</div>
-	<div class="visible-sm">sm</div>
-	<div class="visible-md">md</div>
-	<div class="visible-lg">lg</div>
-	<div class="show-modernizr">&raquo;</div>
 	<pre class="modernizr-features hidden"></pre>
+	<div>
+		<div class="bs-breakpoint">
+			<div class="visible-xxs">xxs</div>
+			<div class="visible-xs hidden-xxs">xs</div>
+			<div class="visible-sm">sm</div>
+			<div class="visible-md">md</div>
+			<div class="visible-lg">lg</div>
+		</div>
+		<div class="modernizr-btn">modernizr</div>
+		<div class="info-window">
+			<div class="info2">Height:<span class="rh-height"></span></div>
+			<div class="info2">Width:<span class="rh-width"></span></div>
+		</div>
+		<div class="info-window">
+			<div class="info1">Scroll:<span class="rh-scroll"></span></div>
+		</div>
+	</div>
 </div>
 <script>
-  $('.show-modernizr').click(function (e) {
+  $('.modernizr-btn').click(function (e) {
       e.preventDefault();
       var features = $('html').attr('class');
       $('.modernizr-features').text(features);
       $('.modernizr-features').toggleClass('hidden');
   });
+  $(window).on('resize scroll', function () {
+      $('.responsive-helper .info-window .rh-height').text($(window).height());
+      $('.responsive-helper .info-window .rh-width').text($(window).width());
+      $('.responsive-helper .info-window .rh-scroll').text($('html').scrollTop());
+  });
+
+  $(window).trigger('resize');
 </script>
