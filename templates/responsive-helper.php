657a8c848ba08f382a3311a08c6847c26e051125
Javier Prieto <jprieton@gmail.com>
2016-01-26 15:35:23 -0430

    Responsive Helper

--- templates/responsive-helper.php
+++ templates/responsive-helper.php
@@ -1,35 +1,40 @@
 <div class="responsive-helper">
 	<pre class="modernizr-features hidden"></pre>
 	<div>
-		<div class="bs-breakpoint">
+		<div class="bs-breakpoint" title="Boostrap breakpoint">
 			<div class="visible-xxs">xxs</div>
 			<div class="visible-xs hidden-xxs">xs</div>
 			<div class="visible-sm">sm</div>
 			<div class="visible-md">md</div>
 			<div class="visible-lg">lg</div>
 		</div>
-		<div class="modernizr-btn">modernizr</div>
-		<div class="info-window">
-			<div class="info2">Height:<span class="rh-height"></span></div>
-			<div class="info2">Width:<span class="rh-width"></span></div>
+		<div class="modernizr-btn" title="moderinzr features">modernizr</div>
+		<div class="info-window" title="Window height & width">
+			<div class="info2">H:<span class="rh-height"></span></div>
+			<div class="info2">W:<span class="rh-width"></span></div>
 		</div>
-		<div class="info-window">
-			<div class="info1">Scroll:<span class="rh-scroll"></span></div>
-		</div>
+		<div class="rh-collapse"><span>&raquo;</span><span>&laquo;</span></div>
 	</div>
 </div>
 <script>
-  $('.modernizr-btn').click(function (e) {
+  jQuery('.modernizr-btn').click(function (e) {
       e.preventDefault();
-      var features = $('html').attr('class');
-      $('.modernizr-features').text(features);
-      $('.modernizr-features').toggleClass('hidden');
+      var features = jQuery('html').attr('class');
+      jQuery('.modernizr-features').text(features);
+      jQuery('.modernizr-features').toggleClass('hidden');
   });
-  $(window).on('resize scroll', function () {
-      $('.responsive-helper .info-window .rh-height').text($(window).height());
-      $('.responsive-helper .info-window .rh-width').text($(window).width());
-      $('.responsive-helper .info-window .rh-scroll').text($('html').scrollTop());
+  jQuery('.rh-collapse').click(function () {
+      jQuery(this).toggleClass('rh-collapsed');
+      if (jQuery('.rh-collapsed').length > 0) {
+          jQuery('.responsive-helper').find('.modernizr-features, .bs-breakpoint, .modernizr-btn, .info-window').addClass('hidden');
+      } else {
+          jQuery('.responsive-helper').find('.bs-breakpoint, .modernizr-btn, .info-window').removeClass('hidden');
+      }
+  });
+  jQuery(window).on('resize scroll', function () {
+      jQuery('.responsive-helper .info-window .rh-height').text(jQuery(window).height());
+      jQuery('.responsive-helper .info-window .rh-width').text(jQuery(window).width());
   });
 
-  $(window).trigger('resize');
+  jQuery(window).trigger('resize');
 </script>
