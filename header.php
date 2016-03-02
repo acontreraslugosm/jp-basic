d2c0937c0e8e16d7c1d4ca1ed1b84bbddb7b8333
Javier Prieto <jprieton@gmail.com>
2016-03-02 14:50:51 -0430

    Bootstrap Navbar

--- header.php
+++ header.php
@@ -1,10 +1,10 @@
 <!doctype html>
 <html <?php language_attributes(); ?> class="no-js">
 	<head>
-		<meta charset="<?php bloginfo('charset') ?>" />
+		<meta charset="<?php bloginfo( 'charset' ) ?>" />
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<meta name="description" content="<?php bloginfo('description') ?>" />
+		<meta name="description" content="<?php bloginfo( 'description' ) ?>" />
 
 		<?php wp_head() ?>
 
@@ -13,4 +13,10 @@
 	<body <?php body_class() ?> itemscope itemtype="http://schema.org/WebPage">
 
 		<?php
-		get_template_part('templates/header');
+		do_action( 'pre_main_content' );
+		get_template_part( 'components/bs-main-navbar' );
+		?>
+
+		<header itemscope itemprop="http://www.schema.org/WPHeader" class="page-header">
+
+		</header>
